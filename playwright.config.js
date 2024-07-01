c246b264743331441289353f74b31b79080f94f4
Naik <dn283@njit.edu>
2024-06-30 22:47:30 -0500

    marking parallel to false

--- playwright.config.js
+++ playwright.config.js
@@ -11,10 +11,10 @@ const { defineConfig, devices } = require('@playwright/test');
  * @see https://playwright.dev/docs/test-configuration
  */
 module.exports = defineConfig({
-  testDir: './tests',
+  testDir: './testsPOM',
   timeout:30000,
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
